da2a349d6f6b2e8549424add75bce205de3c3936
Telma Luīze <telma@luize.com>
2020-09-18 14:01:08 +0300

    Code refactoring to three separate components

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+
 const App = () => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
@@ -10,19 +11,44 @@ const App = () => {
   const part3 = 'State of a component'
   const exercises3 = 14
 
+
+  const Header = () => {
+    return (
+      <div>
+        <h1>{course}</h1>
+      </div>
+    )
+  }
+
+  const Content = () => {
+    return (
+      <div>
+        <p>
+          {part1} {exercises1}
+        </p>
+        <p>
+          {part2} {exercises2}
+        </p>
+        <p>
+          {part3} {exercises3}
+        </p>
+      </div>
+    )
+  }
+
+  const Total = () => {
+    return (
+      <div>
+        <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      </div>
+    )
+
+  }
   return (
     <div>
-<h1>{course}</h1>
-<p>
-  {part1}{exercises1}
-</p>
-<p>
-  {part2}{exercises2}
-</p>
-<p>
-  {part3}{exercises3}
-</p>
-  <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header />
+      <Content />
+      <Total />
     </div>
   )
 }

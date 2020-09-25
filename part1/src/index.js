1ec8dd03d5191d5d181c6e9e4adb195960b2501d
Telma Luīze <telma@luize.com>
2020-09-25 18:20:48 +0300

    Make a single object - with errors

--- part1/src/index.js
+++ part1/src/index.js
@@ -10,7 +10,6 @@ const Header = (props) => {
       <h1> {props.course} </h1>
     </>
   )
-
 }
 
 const Part = (props) => {
@@ -49,23 +48,26 @@ const Total = (props) => {
 
 const App = () => {
 
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+  
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+  
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+  
 
 
   return (

b2314887f4361d7ea2d9d4b14f0d6df98efa7732
Telma Luīze <telma@luize.com>
2020-09-28 19:39:21 +0300

    Create good-bad-neutral components with some funcionality

--- part1/src/index.js
+++ part1/src/index.js
@@ -2,57 +2,84 @@ import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
 
- const Header = ({feedback}) => (
+const Header = ({ feedback }) => (
   <>
-  <h1>{feedback}</h1>
+    <h1>{feedback}</h1>
   </>
- )
- 
- const Button = ({handleClick, text}) => (
-   <>
-   <button onClick={handleClick}>{text}</button>
-   </>
- )
+)
 
- const Statistics = ({statistics, count}) => (
-   <>
-   <h2>{statistics}</h2>
- {/* <p>Good: {count}</p>
-   <p>Bad: {count}</p>
-   <p>Neutral: {count}</p>
+const Button = ({ handleClick, text }) => (
+  <>
+    <button onClick={handleClick}>{text}</button>
+  </>
+)
+
+const Display = ({ counter }) => (
+  <>
+    {counter}
+  </>
+)
+const Good = ({counter}) => (
+  <>
+    <p>Good: {counter}</p>
+    <Display />
+  </>
+)
+
+const Bad = ({counter}) => (
+  <>
+  <p>Bad: {counter}</p>
+  <Display />
+  </>
+)
+
+const Neutral = ({counter}) => (
+  <>
+ <p>Neutral: {counter}</p>
+  <Display />
+  </>
+)
+
+
+const Statistics = ({ statistics }) => (
+  <>
+    <h2>{statistics}</h2>
+    {/*
+   
    <p>All:{count}</p>
    <p>Average: {count}</p>
    <p>Positive: {count}</p> */}
-   </>
- )
-  
- 
+  </>
+)
+
+
 
 const App = () => {
   const feedback = 'Give feedback'
-  const statistics = 'Statistics' 
+  const statistics = 'Statistics'
   // const [count, setCount]  = useState(0)
 
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-  // const increaseByOne = () => setGood(good + 1)
-  // const decreaseByOne = () => setBad(bad - 1)
-  // const doNothing = () => setNeutral(neutral)
+  const increaseByOne = () => setGood(good + 1)
+  const decreaseByOne = () => setBad(bad - 1)
+  const doNothing = () => setNeutral(neutral)
 
-  
+
 
 
   return (
     <>
-        <Header feedback={feedback}/>
-        <Button handleClick={increaseByOne} text='good'/>
-        <Button handleClick={decreaseByOne} text='bad'/>
-        <Button handleClick={doNothing} text='neutral'/>
-        <Statistics statistics={statistics} 
-        // count={}
-        />
+      <Header feedback={feedback} />
+      <Button handleClick={increaseByOne} text='good' />
+      <Button handleClick={decreaseByOne} text='bad' />
+      <Button handleClick={doNothing} text='neutral' />
+      <Statistics statistics={statistics} />
+      <Good counter={good} />
+      <Bad counter={bad} />
+      <Neutral counter={neutral} />
     </>
   )
 }

46b9a94e568050f0efc712b451132e4215e2ef54
Telma Luīze <telma@luize.com>
2020-09-30 12:10:19 +0300

    generate random anecdote

--- part1/src/index.js
+++ part1/src/index.js
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom'
 
 
 const Anecdotes = ({anecdotes}) => (
-  <div>
+ <>
+ <p>
       {anecdotes}
-    </div>
+    </p>
+    </>
 )
 
 const Button = ({handleclick, text}) => (
@@ -17,28 +19,38 @@ const Button = ({handleclick, text}) => (
 )
 
 
-const App = () => {
+const App = ({anecdotes}) => {
+  
   const [selected, setSelected] = useState(0)
-  const anecdotes = [
-    'If it hurts, do it more often',
-    'Adding manpower to a late software project makes it later!',
-    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-    'Premature optimization is the root of all evil.',
-    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-  ]
+  
+  console.log(selected)
+
+  const randomAnecdote = () => {
+    setSelected(
+      Math.floor(Math.random() * Math.floor(anecdotes.length))
+      )
+  }
+    
 
   return (
     <>
     <Anecdotes anecdotes={anecdotes[selected]}/>
-    <Button text='Next anecdote'/>
+    <Button handleclick={randomAnecdote} text='Next anecdote'/>
     </>
   )
 }
 
+const anecdotes = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+]
 
 
 ReactDOM.render(
-  <App  />,
+  <App  anecdotes={anecdotes}/>,
   document.getElementById('root')
 )

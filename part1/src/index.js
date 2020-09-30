e2cec730c076109b56a7566a3f411106d871ab44
Telma Luīze <telma@luize.com>
2020-09-30 12:12:34 +0300

    Same but with data in App

--- part1/src/index.js
+++ part1/src/index.js
@@ -19,7 +19,16 @@ const Button = ({handleclick, text}) => (
 )
 
 
-const App = ({anecdotes}) => {
+const App = () => {
+  
+  const anecdotes = [
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+  ]
   
   const [selected, setSelected] = useState(0)
   
@@ -40,17 +49,7 @@ const App = ({anecdotes}) => {
   )
 }
 
-const anecdotes = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-]
-
-
 ReactDOM.render(
-  <App  anecdotes={anecdotes}/>,
+  <App />,
   document.getElementById('root')
 )

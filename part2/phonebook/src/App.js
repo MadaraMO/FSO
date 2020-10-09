1f8f7a50eb17dc184acd2803f221287fde2a7c53
Telma Luīze <telma@luize.com>
2020-10-09 16:56:03 +0300

    Show persons.name, warning: each child should have key, <ul><li //> doesn't display submitted input

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,26 +1,62 @@
 import React, { useState } from 'react'
 
-const App = () => {
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
-  const [ newName, setNewName ] = useState('')
+const Persons = ({ persons }) => (
+    <>
+    <ul>
+        {persons.map((person) =>
+           <li key={person.id}>{person.name}</li>  
+        )}
+       
+    </ul>
+    </>
+)  
 
-  return (
-    <div>
-      <h2>Phonebook</h2>
-      <form>
+const App = () => {
+    const [persons, setPersons] = useState([
+        { name: 'Arto Hellas' }
+    ])
+    const [newName, setNewName] = useState('Aloha, mermaid...')
+
+    const addName = (e) => {
+        e.preventDefault()
+        const nameObject = {
+            content: newName,
+            id: persons.length + 1,
+        }
+        setPersons(persons.concat(nameObject))
+        setNewName('')
+        console.log(persons.name)
+    }
+
+    const handleNameChange = (e) => {
+        console.log(e.target.value)
+        setNewName(e.target.value)
+    }
+
+
+
+    return (
         <div>
-          name: <input />
+            <h2>Phonebook</h2>
+            <form onSubmit={addName}>
+                <div>
+                    name: <input
+                        value={newName}
+                        onChange={handleNameChange} />
+                </div>
+                <div>
+                    <button type="submit">add</button>
+                </div>
+            </form>
+        
+            <h2>Numbers</h2>
+            <Persons persons={persons}/>
+      
+            <div>debug: {newName}</div>
+           
         </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
-      ...
-    </div>
-  )
+    )
 }
+
 
 export default App

8b972aa170218c87e8bd89db4884d91513da3446
Telma Luīze <telma@luize.com>
2020-10-13 11:53:33 +0300

    2.10 /components

---
+++ part2/phonebook/src/components/Filter.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+const Filter = ({ handleSearchName }) => (
+    <>
+        <p> Name: </p>
+        <input
+            onChange={handleSearchName}
+        />
+    </>
+)
+
+export default Filter
---
+++ part2/phonebook/src/components/PersonForm.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const PersonForm = (
+    {
+        addContact,
+        newName,
+        newNumber,
+        handleNameChange,
+        handleNumberChange }) => (
+
+        <form onSubmit={addContact}>
+            <div>
+                name: <input
+                    value={newName}
+                    onChange={handleNameChange} />
+            </div>
+            <div>
+                number: <input
+                    value={newNumber}
+                    onChange={handleNumberChange} />
+            </div>
+            <div>
+                <button type="submit">add</button>
+            </div>
+        </form>
+    )
+
+export default PersonForm

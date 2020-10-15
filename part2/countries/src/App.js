a8ee30624d6b98da2db0f8b8176c38202aae99f3
Telma Luīze <telma@luize.com>
2020-10-15 12:54:29 +0300

    Countries - conditional rendering (part of 2.12)

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -5,55 +5,71 @@ import axios from 'axios'
 const Filter = ({ handleFilterCountry }) => (
   <>
     <p> Find countries </p>
-    <input onChange={handleFilterCountry}/>
-    
+    <input onChange={handleFilterCountry} />
+
   </>
 )
-const Country = ({ countries }) => (
-  <ul>
-    {
-      countries.map((country) =>
-        <li key={country.name}>{country.name}</li>
-      )}
-  </ul>
-)
 
+const Country = ({ countries }) => {
 
+  if (countries.length > 10) {
+    return (
+      <p>Too many matches, specify another filter</p>
+    )
 
-const App = () => {
-  const [countries, setCountries] = useState([])
-  const [filterCountry, setFilterCountry] = useState('')
-  const [showCountries, setShowCountries] = useState(true)
-
-
-  useEffect(() => {
-    axios
-      .get('https://restcountries.eu/rest/v2/all')
-      .then(response => {
-        setCountries(response.data)
-      })
-  }, [])
-
-
-  const countriesToShow = showCountries
-    ? countries
-    : countries
-      .filter(country =>
-        country.name.toLowerCase()
-          .includes(filterCountry.toLowerCase()))
-  
-
-  const handleFilterCountry = (e) => {
-    setFilterCountry(e.target.value)
-    setShowCountries(false)
+  } else if (countries.length < 10) {
+    return (
+      <ul>
+        {
+          countries.map((country) =>
+            <li key={country.name}>{country.name}</li>
+          )}
+      </ul>
+    )
+    
+  } else {
+    return (
+      <p>Feel free to search for a country</p>
+    )
   }
-  return (
-    <>
-      <h1>Countries</h1>
-      <Filter handleFilterCountry={handleFilterCountry} />
-      <Country countries={countriesToShow} />
-    </>
-  )
 }
 
-export default App;
+
+
+  const App = () => {
+    const [countries, setCountries] = useState([])
+    const [filterCountry, setFilterCountry] = useState('')
+    const [showCountries, setShowCountries] = useState(true)
+
+
+    useEffect(() => {
+      axios
+        .get('https://restcountries.eu/rest/v2/all')
+        .then(response => {
+          setCountries(response.data)
+        })
+    }, [])
+
+
+    const countriesToShow = showCountries
+      ? true
+      : countries
+        .filter(country =>
+          country.name.toLowerCase()
+            .includes(filterCountry.toLowerCase()))
+
+
+    const handleFilterCountry = (e) => {
+      setFilterCountry(e.target.value)
+      setShowCountries(false)
+    }
+    return (
+      <>
+        <h1>Countries</h1>
+        <Filter handleFilterCountry={handleFilterCountry} />
+        <Country countries={countriesToShow} />
+      </>
+    )
+  }
+
+  export default App;

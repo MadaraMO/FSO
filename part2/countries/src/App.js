e19998829c675a20b545dc18a33ae0c38f3dff47
Telma Luīze <telma@luize.com>
2020-10-22 15:22:39 +0300

    Sidetrack:If statement awesomely fine, all else kind a died

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -26,7 +26,7 @@ const Country = ({ country }) => {
         setWeather(response.data.current.condition)
       })
   }, [country])
-  console.log(weather.current)
+
   return (
     <>
       <h3>{country.name}</h3>
@@ -51,17 +51,9 @@ const Country = ({ country }) => {
 
 const List = ({ countriesList, setToCountry }) => {
   // joprojām šis if neiet cauri
-  if (countriesList.length > 10 && !countriesList.length === 1) {
+  if (countriesList.length > 10) {
     return (
       <p>Too many matches, specify another filter</p>
-    )
-  } else if (countriesList.length === 1) {
-    return (
-      <ul>
-        {countriesList.map((country) =>
-          <Country key={country.name} country={country} />
-        )}
-      </ul>
     )
   } else if (countriesList.length <= 10) {
     return (
@@ -70,6 +62,14 @@ const List = ({ countriesList, setToCountry }) => {
           <li key={country.name}>{country.name} <button onClick={() => setToCountry(country.name)}>Show</button></li>
         )}
       </ul>
+    )
+  } else if (countriesList.length === 1) {
+    return (
+      <>
+        {countriesList.map((country) =>
+          <Country key={country.name} country={country} />
+        )}
+      </>
     )
   }
   return (
@@ -99,7 +99,7 @@ const App = () => {
       .filter(country =>
         country.name.toLowerCase()
           .includes(filterCountry.toLowerCase()))
-    : countries
+    : !countries
 
 
   const handleFilterCountry = (e) => {

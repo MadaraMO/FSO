293e6da23127fb94582671d6fec305e65c9a9e11
Telma Luīze <telma@luize.com>
2020-11-26 16:15:58 +0200

    7.6. step3

--- part7/anecdotes/src/components/CreateNew.js
+++ part7/anecdotes/src/components/CreateNew.js
@@ -6,6 +6,7 @@ const CreateNew = ({ addNew, history }) => {
     const content = useField('name')
     const author = useField('author')
     const info = useField('info')
+    const reset = useField('')
 
 
     const handleSubmit = (e) => {
@@ -16,56 +17,50 @@ const CreateNew = ({ addNew, history }) => {
             info: info.value,
             votes: 0
         })
-
         history.push('/')
-    }
-
-    // Tikpat labi var arī atstāt tukšu. pieļauju, ka nevajadzētu, bet, hei, man strādā. kāpēc?
-    const handleReset = () => {
     }
 
     return (
         <div>
             <h2>create a new anecdote</h2>
-            {/* <form onSubmit={handleSubmit}> ar enter tāpat aiziet */}
             <form >
+                {/* <form > */}
                 <div>
                     content
           <input
-                        //   {...content}
-                        name={content.name}
-                        value={content.value}
-                        onChange={content.onChange}
-                        onReset={content.onReset}
+                        {...content}
+                    // name={content.name}
+                    // value={content.value}
+                    // onChange={content.onChange}
+                    // {...content}
                     // ref={input}
                     />
                 </div>
                 <div>
                     author
           <input
-                        //   {...author}
-                        name={author.name}
-                        value={author.value}
-                        onChange={author.onChange}
-                        onReset={author.onReset}
+                        {...author}
+                    // name={author.name}
+                    // value={author.value}
+                    // onChange={author.onChange}
+                    // {...author}
                     // ref={input}
                     />
                 </div>
                 <div>
                     url for more info
           <input
-                        //   {...info}
-                        name={info.name}
-                        value={info.value}
-                        onChange={info.onChange}
-                        onReset={info.onReset}
+                        {...info}
+                    // name={info.name}
+                    // value={info.value}
+                    // onChange={info.onChange}
                     // ref={input}
                     />
                 </div>
                 <button onClick={handleSubmit}>create</button>
-                <button onClick={handleReset}>reset</button>
+                <button onClick={() => reset}>reset</button>
             </form>
-        </div>
+        </div >
     )
 }
 
--- part7/anecdotes/src/hooks/index.js
+++ part7/anecdotes/src/hooks/index.js
@@ -1,21 +1,20 @@
 import { useState } from 'react'
 
-export const useField = (name) => {
+export const useField = ({ name }) => {
     const [value, setValue] = useState('')
 
     const onChange = (e) => {
         setValue(e.target.value)
     }
 
-    // tukšā patrona?
-    const onReset = () => {
+    const reset = () => {
         setValue('')
     }
 
-    return {
-        name,
-        value,
-        onChange,
-        onReset
-    }
+
+
+    return { name, value, onChange, ...reset }
+
+
+
 }

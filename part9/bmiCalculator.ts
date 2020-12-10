27d593afbac3c87298e0f192280f346902b9b366
Telma Luīze <telma@luize.com>
2020-12-10 17:32:17 +0200

    9.3. bmiCalculator.ts

--- part9/bmiCalculator.ts
+++ part9/bmiCalculator.ts
@@ -15,8 +15,29 @@ const calculateBmi = (height: number, weight: number): string => {
     return "Obese";
   }
 };
-// ....atkārtošu exerciseCalculator.ts parsēšanas shēmu
-const height = Number(process.argv[2]);
-const weight = Number(process.argv[3]);
+// ....atkārtoju exerciseCalculator.ts parsēšanas shēmu
 
+const parseBmi = (args: string[]) => {
+  if (args.length < 4) throw new Error("Not enough arguments");
+  if (args.length > 4) throw new Error("Too many arguments");
+
+  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+    return {
+      height: Number(args[2]),
+      weight: Number(args[3]),
+    };
+  } else {
+    throw new Error("Provided values were not numbers!");
+  }
+};
+
+if (process.argv.includes("calculateBmi"))
+  try {
+    const { height, weight } = parseBmi(process.argv);
+    console.log(calculateBmi(height, weight));
+  } catch (e) {
+    console.log("Error, something bad happened, message: ", e.message);
+  }
+
+const { height, weight } = parseBmi(process.argv);
 console.log(calculateBmi(height, weight));

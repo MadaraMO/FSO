05b4927c5b4a178fc1673e0fc66e41a522889dec
Telma Luīze <telma@luize.com>
2020-12-11 16:45:57 +0200

    9.6. Eslint

--- part9/bmiCalculator.ts
+++ part9/bmiCalculator.ts
@@ -13,33 +13,27 @@ export const calculateBmi = (height: number, weight: number): string => {
   }
 };
 
-  // else {
-  //   // bija kļūda "Function lacks ending return statement",
-  //   throw new Error("Malformatted parameters");
-  // }
+const parseBmi = (args: string[]) => {
+  if (args.length < 4) throw new Error("Not enough arguments");
+  if (args.length > 4) throw new Error("Too many arguments");
 
+  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+    return {
+      height: Number(args[2]),
+      weight: Number(args[3]),
+    };
+  } else {
+    throw new Error("Provided values were not numbers!");
+  }
+};
 
-// const parseBmi = (args: string[]) => {
-//   if (args.length < 4) throw new Error("Not enough arguments");
-//   if (args.length > 4) throw new Error("Too many arguments");
-
-//   if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
-//     return {
-//       height: Number(args[2]),
-//       weight: Number(args[3]),
-//     };
-//   } else {
-//     throw new Error("Provided values were not numbers!");
-//   }
-// };
-
-// if (process.argv.includes("calculateBmi"))
-//   try {
-//     const { height, weight } = parseBmi(process.argv);
-//     console.log(calculateBmi(height, weight));
-//   } catch (e) {
-//     console.log("Error, something bad happened, message: ", e.message);
-//   }
+if (process.argv.includes("calculateBmi"))
+  try {
+    const { height, weight } = parseBmi(process.argv);
+    console.log(calculateBmi(height, weight));
+  } catch (e) {
+    console.log("Error, something bad happened");
+  }
 
 // const { height, weight } = parseBmi(process.argv);
 // console.log(calculateBmi(height, weight));
--- part9/exerciseCalculator.ts
+++ part9/exerciseCalculator.ts
@@ -14,14 +14,14 @@ interface TrainingHours {
   hours: number[];
 }
 
-// šīs divas rindas vispār visiem kopētas no FSO.  
+// šīs divas rindas vispār visiem kopētas no FSO.
 // args būs masīvs ar ... simbolu virkni, atgriezīsim augstākminēto interface
 // throw neesmu apguvusi, bet redzams - ja args simbolu virkne ir mazāka par prasīto, tiek izmesta kļūda
 const parseHours = (args: Array<string>): TrainingHours => {
   if (args.length < 4) throw new Error("Not enough arguments");
 
   // nav mans, nebūtu izdomājusi, slice? runa ir par masīvu, jā.
-  // šajā zonā es arī kāros, jo man nekādi neizdevās realizēt 
+  // šajā zonā es arī kāros, jo man nekādi neizdevās realizēt
   // const thisorthat = process.argv[3] kā izmantojamu vērtību
   const trainingHours = process.argv.slice(3);
 
@@ -34,7 +34,7 @@ const parseHours = (args: Array<string>): TrainingHours => {
   if (
     !isNaN(Number(target)) &&
     // par every nebūtu iedomājusies. Neesmu to izmantojusi.
-    // hmm... man vajadzēja gan iedomāties par to, ka jāizmanto kāda Array method, duuh... 
+    // hmm... man vajadzēja gan iedomāties par to, ka jāizmanto kāda Array method, duuh...
     trainingHours.every((arg) => !isNaN(Number(arg)))
   ) {
     return {
@@ -86,16 +86,16 @@ const calculateExercises = (target: number, hours: number[]): ExerciseInfo => {
 
 // es nemāku process.argv. man jāpamācās. ja mācētu, varētu iedomāties par metožu izmantošanu
 if (process.argv.includes("exerciseCalculator"))
-// try arī es pagaidām esmu izlaidusi
+  // try arī es pagaidām esmu izlaidusi
   try {
     // destrukturējam zagto parsēšanas metodi, lai izgūtu divus atsevišķus mainīgos
     // ... un vēl te args pārtop process.argv
     const { target, hours } = parseHours(process.argv);
     console.log(calculateExercises(target, hours));
   } catch (e) {
-    console.log("Error, something bad happened, message: ", e.message);
+    console.log("Error, something bad happened");
   }
 
-  // ja šo neatkārto, tad konsole neko neuzrāda
+// ja šo neatkārto, tad konsole neko neuzrāda
 const { target, hours } = parseHours(process.argv);
 console.log(calculateExercises(target, hours));

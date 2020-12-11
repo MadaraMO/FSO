c64e2312963cbac8e17819fbd3e7cbaa9b6a33a5
Telma Luīze <telma@luize.com>
2020-12-11 15:38:22 +0200

    9.5.

--- part9/bmiCalculator.ts
+++ part9/bmiCalculator.ts
@@ -1,42 +1,45 @@
-const calculateBmi = (height: number, weight: number): string => {
+export const calculateBmi = (height: number, weight: number): string => {
   const heightSquare = (height / 100) ** 2;
   const result = weight / heightSquare;
 
   if (result > 16 && result < 18.5) {
     return "Underweight";
-  }
-  if (result > 18.5 && result < 25) {
+  } else if (result > 18.5 && result < 25) {
     return "Normal";
-  }
-  if (result > 25 && result < 30) {
+  } else if (result > 25 && result < 30) {
     return "Overweight";
-  }
-  if (result >= 30) {
+  } else {
     return "Obese";
   }
 };
 
-const parseBmi = (args: string[]) => {
-  if (args.length < 4) throw new Error("Not enough arguments");
-  if (args.length > 4) throw new Error("Too many arguments");
+  // else {
+  //   // bija kļūda "Function lacks ending return statement",
+  //   throw new Error("Malformatted parameters");
+  // }
 
-  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
-    return {
-      height: Number(args[2]),
-      weight: Number(args[3]),
-    };
-  } else {
-    throw new Error("Provided values were not numbers!");
-  }
-};
 
-if (process.argv.includes("calculateBmi"))
-  try {
-    const { height, weight } = parseBmi(process.argv);
-    console.log(calculateBmi(height, weight));
-  } catch (e) {
-    console.log("Error, something bad happened, message: ", e.message);
-  }
+// const parseBmi = (args: string[]) => {
+//   if (args.length < 4) throw new Error("Not enough arguments");
+//   if (args.length > 4) throw new Error("Too many arguments");
 
-const { height, weight } = parseBmi(process.argv);
-console.log(calculateBmi(height, weight));
+//   if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+//     return {
+//       height: Number(args[2]),
+//       weight: Number(args[3]),
+//     };
+//   } else {
+//     throw new Error("Provided values were not numbers!");
+//   }
+// };
+
+// if (process.argv.includes("calculateBmi"))
+//   try {
+//     const { height, weight } = parseBmi(process.argv);
+//     console.log(calculateBmi(height, weight));
+//   } catch (e) {
+//     console.log("Error, something bad happened, message: ", e.message);
+//   }
+
+// const { height, weight } = parseBmi(process.argv);
+// console.log(calculateBmi(height, weight));
--- part9/index.ts
+++ part9/index.ts
@@ -1,8 +1,29 @@
 import express = require("express");
+import { calculateBmi } from "./bmiCalculator";
 
 const app = express();
 app.get("/hello", (_req, res) => {
   res.send("Hello Full Stack!");
+});
+
+app.get("/bmi", (req, res) => {
+  const { height, weight } = req.query;
+
+  const calculationHeight = Number(height);
+  const calculationWeight = Number(weight);
+  const evaluation = calculateBmi(calculationHeight, calculationWeight);
+
+  if (!isNaN(calculationHeight) && !isNaN(calculationWeight)) {
+    res.json({
+      weight: weight,
+      height: height,
+      bmi: evaluation,
+    });
+  } else {
+    res.json({
+      error: "malformatted parameters",
+    });
+  }
 });
 
 const PORT = 3003;

bd0d781b6f87a9491e56d80f2645f2b719bcf1da
Telma Luīze <telma@luize.com>
2020-12-14 13:11:20 +0200

    9.7. WebExercises

--- part9/index.ts
+++ part9/index.ts
@@ -1,7 +1,12 @@
 import express = require("express");
 import { calculateBmi } from "./bmiCalculator";
+import { calculateExercises } from "./exerciseCalculator";
+// šis man ienāca automātiski
+import { isArray } from "util";
 
 const app = express();
+app.use(express.json());
+
 app.get("/hello", (_req, res) => {
   res.send("Hello Full Stack!");
 });
@@ -24,6 +29,30 @@ app.get("/bmi", (req, res) => {
       error: "malformatted parameters",
     });
   }
+});
+
+app.post("/exercises", (req, res) => {
+  // neesmu pārliecināta
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const { daily_exercises, target }: any = req.body;
+
+//   neizdevās pielietot
+  //   const bodyDailyExercises = Array<number>(daily_exercises);
+
+  const bodyTarget = Number(target);
+
+  if (!daily_exercises || !target)
+    return res.status(400).json({ error: "parameters missing" });
+  if (isNaN(bodyTarget) || !isArray(daily_exercises))
+    return res.status(400).json({ error: "malformatted parameters" });
+
+  // šo gribētu glītāk
+  daily_exercises.map((ex) => {
+    if (isNaN(Number(ex)))
+      res.status(400).json({ error: "malformatted parameters" });
+  });
+
+  return res.json(calculateExercises(daily_exercises, target));
 });
 
 const PORT = 3003;

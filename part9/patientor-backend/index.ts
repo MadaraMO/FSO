b78854f6092db3fcbdd58678faa87b2d4ea462be
Telma Luīze <telma@luize.com>
2020-12-15 19:37:01 +0200

    9.9.

--- part9/patientor-backend/index.ts
+++ part9/patientor-backend/index.ts
@@ -1,14 +1,20 @@
-import express from "express";
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import express = require("express");
+import cors = require("cors");
+
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 
-const PORT = 3000;
-
-app.get("/ping", (_req, res) => {
+app.get("/api/ping", (_req, res) => {
   console.log("someone pinged here");
-  res.send("pong");
+  res.json("pong");
 });
 
+const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });

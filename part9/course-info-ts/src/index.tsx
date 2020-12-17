a964d5a898100dbfbac2964f781ee3e5d7334f6f
Telma Luīze <telma@luize.com>
2020-12-18 00:10:31 +0200

    9.15.

--- part9/course-info-ts/src/index.tsx
+++ part9/course-info-ts/src/index.tsx
@@ -1,26 +1,61 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { CoursePart } from "./types";
+
+const assertNever = (value: never): never => {
+  throw new Error(
+    `Unhandled discriminated union member: ${JSON.stringify(value)}`
+  );
+};
 
 const Header: React.FC<{ name: string }> = ({ name }) => <h1> {name}</h1>;
 
-const Part: React.FC<{ name: string; exercises: number }> = ({
-  name,
-  exercises,
-}) => (
-  <p>
-    {name} {exercises}
-  </p>
-);
+const Part: React.FC<{ parts: CoursePart }> = ({ parts }) => {
+  switch (parts.name) {
+    case "Fundamentals":
+      return (
+        <div>
+          <h2> {parts.name} </h2>
+          <p>Exercises: {parts.exerciseCount}</p>
+          <p>Description: {parts.description}</p>
+        </div>
+      );
 
-interface CoursePart {
-  name: string;
-  exerciseCount: number;
-}
+    case "Using props to pass data":
+      return (
+        <div>
+          <h2> {parts.name} </h2>
+          <p>Exercises: {parts.exerciseCount}</p>
+          <p>Group project count: {parts.groupProjectCount}</p>
+        </div>
+      );
+    case "Deeper type usage":
+      return (
+        <div>
+          <h2> {parts.name} </h2>
+          <p>Exercises: {parts.exerciseCount}</p>
+          <p>Description: {parts.description}</p>
+          <p>Submission link: {parts.exerciseSubmissionLink}</p>
+        </div>
+      );
+    case "This is the real deal":
+      return (
+        <div>
+          <h2> {parts.name} </h2>
+          <p>Exercises: {parts.exerciseCount}</p>
+          <p>Description: {parts.description}</p>
+          <p>Submission link: {parts.exerciseSubmissionLink}</p>
+        </div>
+      );
+    default:
+      return assertNever(parts);
+  }
+};
 
 const Content: React.FC<{ parts: CoursePart[] }> = ({ parts }) => (
   <>
-    {parts.map((part) => (
-      <Part name={part.name} exercises={part.exerciseCount} key={part.name} />
+    {parts.map((part: CoursePart) => (
+      <Part parts={part} key={part.name} />
     ))}
   </>
 );
@@ -33,18 +68,29 @@ const Total: React.FC<{ parts: CoursePart[] }> = ({ parts }) => {
 
 const App: React.FC = () => {
   const courseName = "Half Stack application development";
-  const courseParts = [
+  const courseParts: CoursePart[] = [
     {
       name: "Fundamentals",
       exerciseCount: 10,
+      description: "This is an awesome course part",
     },
     {
       name: "Using props to pass data",
       exerciseCount: 7,
+      groupProjectCount: 3,
     },
     {
       name: "Deeper type usage",
       exerciseCount: 14,
+      description: "Confusing description",
+      exerciseSubmissionLink: "https://fake-exercise-submit.made-up-url.dev",
+    },
+    {
+      name: "This is the real deal",
+      exerciseCount: 98,
+      description: "Fire walk with me",
+      exerciseSubmissionLink:
+        "https://en.wikipedia.org/wiki/Twin_Peaks:_Fire_Walk_with_Me",
     },
   ];
 
@@ -52,8 +98,8 @@ const App: React.FC = () => {
     <div>
       <Header name={courseName} />
       <Content parts={courseParts} />
-      <Total parts={courseParts}/>
-    </div> 
+      <Total parts={courseParts} />
+    </div>
   );
 };
 

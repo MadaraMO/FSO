6a357481bde69dfe05053c9a67ae1e30c9eab468
Telma Luīze <telma@luize.com>
2020-12-20 21:26:46 +0200

    9.17.-9.18. at last?

---
+++ part9/patientor-frontend/src/App.tsx
@@ -0,0 +1,62 @@
+// viena no ieteiktajām lietām,
+// lai atrisinātu JSX element type
+// ['BrowserRouter', 'Table', 'Button' etc] is not a constructor function for JSX elements.
+// šis nepalīdzēja, taču tāpat atstāju.
+// palīdzēja - yarn.lock un node_modules izdzēšana un atjaunošana. MILZĪGS LAIKA ZAGLIS
+import * as React from "react";
+import axios from "axios";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import { Button, Divider, Header, Container } from "semantic-ui-react";
+
+import { apiBaseUrl } from "./constants";
+import { useStateValue } from "./state";
+import { Patient } from "./types";
+
+import PatientListPage from "./PatientListPage";
+import PatientPage from "./PatientPage";
+
+import { setPatientList } from "./state/reducer";
+
+const App: React.FC = () => {
+  const [, dispatch] = useStateValue();
+  React.useEffect(() => {
+    axios.get<void>(`${apiBaseUrl}/ping`);
+
+    const fetchPatientList = async () => {
+      try {
+        const { data: patientListFromApi } = await axios.get<Patient[]>(
+          `${apiBaseUrl}/patients`
+        );
+        dispatch(setPatientList(patientListFromApi));
+      } catch (e) {
+        console.error(e);
+      }
+    };
+
+    fetchPatientList();
+  }, [dispatch]);
+
+  // VISPĀR SĀKUMĀ ES GRIBĒJU FETCHOT SINGLEPATIENT DATA NO APP.TSX
+
+  return (
+    <div className="App">
+      <Router>
+        <Container>
+          <Header as="h1">Patientor</Header>
+          <Button as={Link} to="/" primary>
+            Home
+          </Button>
+          <Divider hidden />
+          <Switch>
+            <Route path="/patients/:id" component={PatientPage} />
+            {/* Bija iekritiens. Ar šo zemāk man neizdevās iegūt view, ar to augstāk, ko norakstīju, izdevās. ReactRouter knifi? */}
+            {/* <Route path="patients/:id" render={() => <PatientPage />} /> */}
+            <Route exact path="/" render={() => <PatientListPage />} />
+          </Switch>
+        </Container>
+      </Router>
+    </div>
+  );
+};
+
+export default App;
---
+++ part9/patientor-frontend/src/state/reducer.ts
@@ -0,0 +1,71 @@
+import { State } from "./state";
+import { Patient } from "../types";
+
+export type Action =
+  | {
+      type: "SET_PATIENT_LIST";
+      payload: Patient[];
+    }
+  | {
+      type: "ADD_PATIENT";
+      payload: Patient;
+    }
+  | {
+      type: "SET_SINGLE_PATIENT";
+      payload: Patient;
+    };
+
+export const reducer = (state: State, action: Action): State => {
+  switch (action.type) {
+    case "SET_PATIENT_LIST":
+      return {
+        ...state,
+        patients: {
+          ...action.payload.reduce(
+            (memo, patient) => ({ ...memo, [patient.id]: patient }),
+            {}
+          ),
+          ...state.patients,
+        },
+      };
+    case "ADD_PATIENT":
+      return {
+        ...state,
+        patients: {
+          ...state.patients,
+          [action.payload.id]: action.payload,
+        },
+      };
+    // šo es noskatījos tīmeklī
+    case "SET_SINGLE_PATIENT":
+      return {
+        ...state,
+        singlePatient: {
+          ...action.payload,
+        },
+      };
+    default:
+      return state;
+  }
+};
+
+export const setPatientList = (patientListFromApi: Patient[]): Action => {
+  return {
+    type: "SET_PATIENT_LIST",
+    payload: patientListFromApi,
+  };
+};
+
+export const addPatient = (newPatient: Patient): Action => {
+  return {
+    type: "ADD_PATIENT",
+    payload: newPatient,
+  };
+};
+
+export const setSinglePatient = (singlePatient: Patient): Action => {
+  return {
+    type: "SET_SINGLE_PATIENT",
+    payload: singlePatient,
+  };
+};
